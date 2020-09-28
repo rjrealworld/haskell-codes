bf833a8869a2b0e376da972027777c8f2d76f070
Riya Jain <55906793+rjrealworld@users.noreply.github.com>
2020-09-28 14:14:57 +0530

    Create designerPdfViewer.hs

---
+++ designerPdfViewer.hs
@@ -0,0 +1,17 @@
+import Data.List
+import Data.Char
+
+position :: Char -> Int
+position ch = (fromEnum 'a') - (fromEnum ch)
+
+tallest :: [Char] -> [Int] -> Int
+tallest word letterHeights = maximum $ [letterHeights !! (position ch) | ch <- word]
+
+atoi :: String -> Int
+atoi s = read s :: Int
+
+main = do
+    input <- getLine
+    word <- getLine
+    let answer = (length word) * (tallest word $ map atoi $ words input)
+    print $ answer
