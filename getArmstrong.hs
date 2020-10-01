152e9efbd73a8614d6442cb7593fafb91be2f4f5
Riya Jain <55906793+rjrealworld@users.noreply.github.com>
2020-10-01 12:11:33 +0530

    Update getArmstrong.hs

--- getArmstrong.hs
+++ getArmstrong.hs
@@ -7,7 +7,7 @@ isArmstrong n = n == sum (map (^p) d)
                                where d = num2Digits n
                                      p = length d
 getArmstrong :: Int -> Int -> [Int]
-getArmstrong a b = filter isArmstrong [a..b]
+getArmstrong a b = filter isArmstrong [a . . b]
 
 main = do
     print $ getArmstrong 100 100000
